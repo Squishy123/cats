dd269396f5618dcecd057c7640a8c065daa32f60
Christian Wang <christian.gnaw@gmail.com>
2017-11-06 00:35:31 -0500

    Fixed scrollbar and link

--- js/index.js
+++ js/index.js
@@ -1,1 +1,17 @@
-let main = new Dragend(document.getElementById("main"), {pageClass:"swipe-page", direction: "horizontal", disableScroll : true});
+let main = document.getElementById("main");
+let m = new Dragend(document.getElementById("main"), {
+  pageClass: "swipe-page",
+  direction: "horizontal",
+  disableScroll: true,
+});
+  console.log(m)
+let keys = [];
+
+window.addEventListener("keydown", function(e) {
+
+  keys[e.which] = true;
+  if (keys[39])
+    main.dragend("right");
+  else if (keys[37])
+    main.dragend("left");
+});
