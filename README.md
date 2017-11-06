d554bcca243afb9c2db467ba7f1a6f8ef3798617
Christian Wang <christian.gnaw@gmail.com>
2017-11-05 19:28:05 -0500

    Added dragend lib and added some pages

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # cats
 Where did the cats go? A splash page for my biology project
+
+Visit here https://squishy123.github.io/cats/
---
+++ js/index.js
@@ -0,0 +1,1 @@
+let main = new Dragend(document.getElementById("main"), {pageClass:"swipe-page", direction: "horizontal", disableScroll : true});
